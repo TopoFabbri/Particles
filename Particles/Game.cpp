ed61ad05030b67afb9ad96d49980d4bd4b2b7a88
FewZ2372 <Felipebxeneize@gmail.com>
2022-09-07 19:33:53 -0300

    Draw Particles

--- Particles/Game.cpp
+++ Particles/Game.cpp
@@ -1,7 +1,68 @@
 #include "Game.h"
 
+void Game::update()
+{
+
+
+}
+
+void Game::draw()
+{
+	for (int i = 0; i < partCount; i++)
+	{
+		DrawPixelV(part[i].pos, WHITE);
+	}
+}
 
 void Game::play()
 {
+
+
+}
+
+Game::Game()
+{
+	for (int i = 0; i < partCount; i++)
+	{
+
+		part[i] = setParticle();
+
+	}
+
+}
+Game::~Game()
+{
+
+
+
+}
+
+void Game::loop()
+{
+	while (!WindowShouldClose())
+	{
+		//Update
+		update();
+
+
+		//Draw
+		draw();
+	}
+
+}
+
+Particle setParticle()
+{
+	Particle particle;
+
+	particle.pos.x = rand() % GetScreenWidth();
+	particle.pos.y = rand() % GetScreenHeight();
+
+	particle.velocity.x = 0.0f;
+	particle.velocity.y = 0.0f;
+
+	particle.weight = 1.0f;
+
+	return particle;
 
 }
--- Particles/Game.h
+++ Particles/Game.h
@@ -2,14 +2,31 @@
 #include "Src/Topo.h"
 #include "Planet.h"
 
+struct Particle
+{
+	Vector2 pos;
+	Vector2 velocity;
+
+	float weight;
+};
+
 class Game
 {
 private:
-	char board[10][10];
+
+	static const int partCount = 10000;
+
+	Particle part[partCount];
 
 public:
 	Game();
 	~Game();
 
+	void loop();
 	void play();
+	void update();
+	void draw();
 };
+
+
+Particle setParticle();
